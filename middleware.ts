9b7d6d3784137a847329967bd8dc34b2e00acdf5
Shivam Tiwari <st19932000@gmail.com>
2024-07-08 00:06:33 +0530

    Authenticate 2

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,9 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default authMiddleware({
+    publicRoutes:['/']
+});
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.\\..|_next).)", "/", "/(api|trpc)(.)"],
 };
