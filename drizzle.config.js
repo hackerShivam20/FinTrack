381b3e08759147b9ec145bb26f072ffbb737f294
Shivam Tiwari <st19932000@gmail.com>
2024-07-08 02:53:46 +0530

    Drizzle and neon setup

---
+++ drizzle.config.js
@@ -0,0 +1,11 @@
+// import type { Config } from "drizzle-kit";
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env.local' });
+
+export default {
+    schema: "./utils/schema.jsx",
+    dialect: 'postgresql',
+    dbCredentials: {
+      url: process.env.NEXT_PUBLIC_DATABASE_URL,
+    }
+  };
