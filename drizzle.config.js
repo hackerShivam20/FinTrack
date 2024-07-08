731cf002c370b9cfb127024822f4105694851ce7
Shivam Tiwari <st19932000@gmail.com>
2024-07-08 23:58:49 +0530

    Correction

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,11 +1,10 @@
-// import type { Config } from "drizzle-kit";
+// import type { Config } from 'drizzle-kit';
 import dotenv from 'dotenv';
-dotenv.config({ path: '.env.local' });
-
+dotenv.config({ path: '.env.local' });
 export default {
     schema: "./utils/schema.jsx",
     dialect: 'postgresql',
     dbCredentials: {
       url: process.env.NEXT_PUBLIC_DATABASE_URL,
     }
-  };
+   }; //satisfies Config;
--- utils/dbConfig.jsx
+++ utils/dbConfig.jsx
@@ -1,6 +1,5 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from './schema'
-
-const sql = neon('process.env.NEXT_PUBLIC_DATABASE_URL');
-const db = drizzle(sql,{schema});
+import * as schema from './schema';
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
+const db=drizzle(sql,{schema});
