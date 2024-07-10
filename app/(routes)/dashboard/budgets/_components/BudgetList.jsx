4f3efc938e047ce48b5e8f97d69b775aef138aca
Shivam Tiwari <st19932000@gmail.com>
2024-07-10 22:41:13 +0530

    Skeleton effect and some modification

--- app/(routes)/dashboard/budgets/_components/BudgetList.jsx
+++ app/(routes)/dashboard/budgets/_components/BudgetList.jsx
@@ -2,10 +2,11 @@
 import React, { useEffect, useState } from 'react'
 import CreateBudget from './CreateBudget'
 import { db } from '@/utils/dbConfig'
-import { eq, getTableColumns,sql } from 'drizzle-orm'
+import { desc, eq, getTableColumns,sql } from 'drizzle-orm'
 import { Budgets,Expenses } from '@/utils/schema'
 import { useUser } from '@clerk/nextjs'
 import BudgetItems from './BudgetItems'
+// import { index } from 'drizzle-orm/mysql-core'
 
 function BudgetList(){
 
@@ -26,17 +27,25 @@ function BudgetList(){
     }).from(Budgets)
     .leftJoin(Expenses,eq(Budgets.id,Expenses.budgetId))
     .where(eq(Budgets.createdBy,user?.primaryEmailAddress?.emailAddress))
-    .groupBy(Budgets.id);
+    .groupBy(Budgets.id)
+    .orderBy(desc(Budgets.id));
     setBudgetList(result);
   }
 
   return (
     <div className='mt-7'>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-            <CreateBudget />
-            {BudgetList.map((budget,index)=>(
+            <CreateBudget
+            refreshData={()=>getBudgetList()} />
+            {BudgetList?.length > 0? BudgetList.map((budget,index)=>(
               <BudgetItems budget={budget} />
-            ))}
+            ))
+          :[1,2,3,4,5].map((item, index)=>(
+            <div key={index} className='w-full bg-slate-200 rounded-lg has-[140px] animate-pulse'>
+
+            </div>
+          ))
+          }
         </div>
     </div>
   )
