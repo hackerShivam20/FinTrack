4a7d890b3e9f676e3ae1f508a34a80227d66f121
Shivam Tiwari <st19932000@gmail.com>
2024-07-13 02:43:46 +0530

    Dashboard is ready except bar

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+"use client"
+import React, { useEffect, useState } from 'react'
+import { UserButton, useUser } from '@clerk/nextjs';
+import CardInfo from './_components/CardInfo';
+import { db } from '@/utils/dbConfig';
+import { desc, eq, getTableColumns, sql } from 'drizzle-orm';
+import { Budgets,Expenses } from '@/utils/schema'
 
-export default function Dashboard() {
+export default function Dashboard(){
+  // const {user}=useUser();
+
+  const [BudgetList,setBudgetList]=useState([]);
+  const {user}=useUser();
+  useEffect(()=>{
+    user&&getBudgetList();
+  },[user])
+
+  // used to get budget list
+
+  const getBudgetList=async()=>{
+
+    const result=await db.select({
+      ...getTableColumns(Budgets),
+      totalSpend:sql `sum(${Expenses.amount})`.mapWith(Number),
+      totalItem:sql `count(${Expenses.id})`.mapWith(Number)
+    }).from(Budgets)
+    .leftJoin(Expenses,eq(Budgets.id,Expenses.budgetId))
+    .where(eq(Budgets.createdBy,user?.primaryEmailAddress?.emailAddress))
+    .groupBy(Budgets.id)
+    .orderBy(desc(Budgets.id));
+    setBudgetList(result);
+  }
+
   return (
-    <div className="p-4 md:p-8">
-      This is dashboard
+    <div className="p-5">
+      <h2 className='font-bold text-3xl'>Hi, {user?.fullName} ✌️</h2>
+      <p className='font-bold text-gray-500'>Effortlessly manage your finances with our intuitive expense tracker web app—track spending, set budgets, and gain insights all in one place. Simplify your money management today!</p>
+
+      <CardInfo budgetList={BudgetList}/>
     </div>
   );
 }
