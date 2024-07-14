c7e51e7d96b494974d076ec01d015bbfec847624
Shivam Tiwari <st19932000@gmail.com>
2024-07-15 03:18:48 +0530

    Add name in the side of logo

--- app/(routes)/dashboard/_components/DashboardHeader.jsx
+++ app/(routes)/dashboard/_components/DashboardHeader.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
-import { UserButton } from '@clerk/nextjs'
+// import { UserButton } from '@clerk/nextjs'
+import { UserButton, useUser } from '@clerk/nextjs'
+
 function DashboardHeader() {
+  const { user, isSignedIn } = useUser();
+
   return (
     <div className='p-5 shadow-sm border-b flex justify-between'>
         <div>
             
         </div>
         <div>
-            
+            {/* Display user name if signed in */}
+            {isSignedIn && user?.firstName && (
+                <span className='mr-3 font-bold'>
+                    Welcome, {user.firstName}
+                </span>
+            )}
             <UserButton />
         </div>
         
--- app/_component/Header.jsx
+++ app/_component/Header.jsx
@@ -4,27 +4,36 @@ import Image from 'next/image'
 import React from 'react'
 import { UserButton, useUser } from '@clerk/nextjs'
 import Link from 'next/link';
+
 function Header() {
-    const {user,isSignedIn}=useUser();
+    const { user, isSignedIn } = useUser();
 
     return (
-      <div className='p-3 flex justify-between items-center border shadow-sm'>
-        {/* yahan pe ye image sahi karna hai */}
-        <Image src={'./logo.svg'}
-        alt='logo'
-        width={50}
-        height={100}
-        />
-        {
-          isSignedIn ?
-            <UserButton/> :
-            <Link href={'/sign-in'}>
-              <Button>Sign In</Button>
-            </Link>
-        }
-        
-      </div>
+        <div className='p-3 flex justify-between items-center border shadow-sm'>
+            {/* Logo Image */}
+            <Image src={'/logo.svg'}
+                alt='logo'
+                width={50}
+                height={100}
+            />
+            <div className='flex items-center'>
+                {/* Display user name if signed in */}
+                {isSignedIn && user?.firstName && (
+                    <span className='mr-3 font-bold'>
+                        Welcome, {user.firstName}
+                    </span>
+                )}
+                {/* Sign-in button or user button */}
+                {isSignedIn ? (
+                    <UserButton />
+                ) : (
+                    <Link href={'/sign-in'}>
+                        <Button>Sign In</Button>
+                    </Link>
+                )}
+            </div>
+        </div>
     )
 }
 
-export default Header
+export default Header;
