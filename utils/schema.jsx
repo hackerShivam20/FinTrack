323d6bee85168ea154fb36c34069052569e600b9
Shivansh <greatshiva773@gmail.com>
2024-07-15 02:51:22 +0530

    ADD Bill reminder

--- utils/schema.jsx
+++ utils/schema.jsx
@@ -1,4 +1,5 @@
-import { pgTable,serial,varchar,integer,numeric } from "drizzle-orm/pg-core";
+import { pgTable,serial,varchar,integer,numeric,boolean ,timestamp} from "drizzle-orm/pg-core";
+import { text, date } from 'drizzle-orm/sqlite-core';
 
 export const Budgets=pgTable('budgets',{
     id:serial('id').primaryKey(),
@@ -15,3 +16,12 @@ export const Expenses=pgTable('expenses',{
     budgetId:integer('budgetId').references(()=>Budgets.id),
     createdAt:varchar('created_at').notNull(),
 })
+
+export const Bills = pgTable('bills', {
+    id: serial('id').primaryKey(),
+    name: varchar('name').notNull(),
+    amount: numeric('amount').notNull(),
+    dueDate: varchar('due_date').notNull(),
+    isPaid: boolean('is_paid').notNull().default(false),
+    createdAt: timestamp('created_at').notNull().defaultNow()
+});
