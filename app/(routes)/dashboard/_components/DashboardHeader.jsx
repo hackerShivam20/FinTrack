23a01e70ef94f31d1a54ea11fd122440252c6705
Shivam Tiwari <st19932000@gmail.com>
2024-07-16 00:10:27 +0530

    Going top arrow button

--- app/(routes)/dashboard/_components/DashboardHeader.jsx
+++ app/(routes)/dashboard/_components/DashboardHeader.jsx
@@ -1,27 +1,48 @@
-import React from 'react'
-// import { UserButton } from '@clerk/nextjs'
-import { UserButton, useUser } from '@clerk/nextjs'
+import React from 'react';
+import { UserButton, useUser } from '@clerk/nextjs';
+import { FaArrowUp } from 'react-icons/fa'; // Import the arrow up icon from react-icons
 
 function DashboardHeader() {
   const { user, isSignedIn } = useUser();
 
   return (
-    <div className='p-5 shadow-sm border-b flex justify-between'>
-        <div>
-            
-        </div>
-        <div>
-            {/* Display user name if signed in */}
-            {isSignedIn && user?.firstName && (
-                <span className='mr-3 font-bold'>
-                    Welcome, {user.firstName}
-                </span>
-            )}
-            <UserButton />
-        </div>
-        
+    <div className='p-5 shadow-sm border-b flex justify-between items-center'>
+      <div>
+        {/* Dashboard Header Content */}
+      </div>
+      <div>
+        {/* Display user name if signed in */}
+        {isSignedIn && user?.firstName && (
+          <span className='mr-3 font-bold'>
+            Welcome, {user.firstName}
+          </span>
+        )}
+        <UserButton />
+      </div>
     </div>
-  )
+  );
 }
 
-export default DashboardHeader
+function App() {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div>
+      <DashboardHeader />
+      {/* Other content of your app */}
+      <button
+        onClick={scrollToTop}
+        className='fixed bottom-4 right-4 bg-primary text-white p-3 rounded-xl shadow-lg flex items-center justify-center'
+      >
+        <FaArrowUp size={20} />
+      </button>
+    </div>
+  );
+}
+
+export default App;
